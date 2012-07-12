d81087898d7cd8c46e5c257fbc6bfa5a18f39963
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-07-13 10:16:06 +1200

    Add maturity level to version.php

--- version.php
+++ version.php
@@ -29,4 +29,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2012061102;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->release   = '1.0 Beta 4';
 $plugin->requires  = 2010112400;        // Requires this Moodle version
+$plugin->maturity  = MATURITY_BETA;
 $plugin->component = 'repository_recordaudio'; // Full name of the plugin (used for diagnostics)
